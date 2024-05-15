6d821635e7532240b05ee01ab3e72306bcbf7396
learning-kyljmeeski <learning.kyljmeeski@gmail.com>
2024-05-16 00:23:41 +0600

    feat: make notification appearance animation more clean and satisfying

--- src/lib/Notification.svelte
+++ src/lib/Notification.svelte
@@ -20,16 +20,35 @@
         border-radius: 5px;
         box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
         z-index: 1000;
+        animation: bounceInRight 0.5s cubic-bezier(0.68, -0.55, 0.27, 1.55); /* Анимация появления */
     }
-    .success {
+
+    .notification.success {
         --background-color: #28a745;
     }
-    .error {
+
+    .notification.error {
         --background-color: #dc3545;
     }
+
     .close {
         margin-left: 15px;
         cursor: pointer;
+    }
+
+    @keyframes bounceInRight {
+        0% {
+            transform: translateX(300px) scaleY(2);
+            opacity: 0;
+        }
+        60% {
+            transform: translateX(-25px) scaleY(0.9);
+            opacity: 1;
+        }
+        100% {
+            transform: translateX(0) scaleY(1);
+            opacity: 1;
+        }
     }
 </style>
 
